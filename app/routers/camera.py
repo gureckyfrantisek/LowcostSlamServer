b0cc6b72b5ff4ca949430a884a2dfd4309f57cb7
František Gurecký <gurecfra@cvut.cz>
2026-04-19 11:38:04 +0200

    Fix camera routes

--- app/routers/camera.py
+++ app/routers/camera.py
@@ -4,11 +4,11 @@ from app.core.camera import *
 
 router = APIRouter()
 
-@router.post("/camera/start-recording")
+@router.post("/start-recording")
 async def start_recording_route():
     return start_recording()
 
 
-@router.post("/camera/stop-recording")
+@router.post("/stop-recording")
 async def stop_recording_route():
     return stop_recording()
